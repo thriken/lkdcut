8a760d5906217e71b8e7e4c6c6e237f9eb61e174
thriken <49754208@qq.com>
2026-06-21 11:06:27 +0800

    Changes to be committed: 	modified:   .gitignore 	deleted:    glass_data.db.bak 	deleted:    notepad4.zip 	modified:   runtoexe.bat 	renamed:    "\347\216\273\347\222\203\345\210\207\345\211\262\346\225\260\346\215\256\347\256\241\347\220\2062026.spec" -> "\347\216\273\347\222\203\345\210\207\345\211\262\346\225\260\346\215\256\347\256\241\347\220\206.spec"

--- runtoexe.bat
+++ runtoexe.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --onefile --windowed --add-data="origin.xls:." --upx-dir "E:\Project\PieceInfo_v2\upx-5.0.2-win64" --clean --name "玻璃切割数据管理2026" "E:\Project\lkdcut\cut.py"
+pyinstaller --noconfirm --onefile --windowed --add-data="origin.xls:." --upx-dir "E:\Project\PieceInfo_v2\upx-5.0.2-win64" --clean --name "玻璃切割数据管理" "E:\Project\lkdcut\cut.py"
--- 玻璃切割数据管理.spec
+++ 玻璃切割数据管理.spec
@@ -22,7 +22,7 @@ exe = EXE(
     a.binaries,
     a.datas,
     [],
-    name='玻璃切割数据管理2026',
+    name='玻璃切割数据管理',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
